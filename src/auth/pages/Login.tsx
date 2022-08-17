72801f780f74b345cafa816355e8ae636a2e33d8
Santiago <santiagobalino@hotmail.com>
2022-08-17 00:25:03 -0300

    feat(Root): Add Login and Register pages

--- src/auth/pages/Login.tsx
+++ src/auth/pages/Login.tsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import {
+  Wrapper,
+  CardLogin,
+  LoginTitle,
+  Form,
+  FormInput,
+  LoginButton,
+  LinkForm,
+} from '../styles/Login.styled';
+
+export interface Input {
+  margin?: string;
+}
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Wrapper>
+      <CardLogin>
+        <LoginTitle>Inicia sesión</LoginTitle>
+        <Form>
+          <FormInput type='email' placeholder='Email' />
+          <FormInput margin='20px' type='password' placeholder='Contraseña' />
+          <LoginButton>Iniciar Sesión</LoginButton>
+        </Form>
+        <LinkForm to='/auth/register'>Registrarse</LinkForm>
+      </CardLogin>
+    </Wrapper>
+  );
 };
--- src/auth/pages/Register.tsx
+++ src/auth/pages/Register.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import {
+  CardLogin,
+  Form,
+  FormInput,
+  LoginLink,
+  LoginTitle,
+  RedirectButton,
+  RegisterButton,
+  Wrapper,
+} from '../styles/Login.styled';
+
+export interface RegButton {
+  margin?: string;
+}
 
 export const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Wrapper>
+      <CardLogin>
+        <LoginTitle>Registrarse</LoginTitle>
+        <Form>
+          <FormInput margin='20px' placeholder='Nombre' />
+          <FormInput margin='20px' type='email' placeholder='Email' />
+          <FormInput margin='20px' type='password' placeholder='Contraseña' />
+          <FormInput
+            margin='20px'
+            type='password'
+            placeholder='Repetir contraseña'
+          />
+          <RegisterButton margin='20px'>Registrarse</RegisterButton>
+        </Form>
+        <RedirectButton>
+          ¿Ya tienes una cuenta?
+          <LoginLink to='/auth/login'>Inicia sesión</LoginLink>
+        </RedirectButton>
+      </CardLogin>
+    </Wrapper>
+  );
 };
