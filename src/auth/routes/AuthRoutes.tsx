6fffb84cddcd64fa333e6cc5fc342d581ff3ae07
Santiago <santiagobalino@hotmail.com>
2022-08-16 23:56:18 -0300

    feat(Root): Add routes to the page

--- src/auth/routes/AuthRoutes.tsx
+++ src/auth/routes/AuthRoutes.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Login } from '../pages/Login';
+import { Register } from '../pages/Register';
 
 export const AuthRoutes = () => {
-  return <div>AuthRoutes</div>;
+  return (
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='register' element={<Register />} />
+
+      <Route path='/*' element={<Navigate to='/auth/login' />} />
+    </Routes>
+  );
 };
--- src/components/routes/HomeRoutes.tsx
+++ src/components/routes/HomeRoutes.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Homepage } from '../Homepage';
 
 export const HomeRoutes = () => {
-  return <div>HomeRoutes</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<Homepage />} />
+      <Route path='/*' element={<Navigate to='/' />} />
+    </Routes>
+  );
 };
